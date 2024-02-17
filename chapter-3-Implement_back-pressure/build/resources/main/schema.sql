c7a3c179ace684ca1c95f3e8d394b5faa3dc8a40
Ahmet Meric <ahmeric@gmail.com>
2024-02-17 18:46:39 +0000

    build folders are cleaned

--- chapter-3-Implement_back-pressure/build/resources/main/schema.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE IF NOT EXISTS users (
-                                     id INT PRIMARY KEY AUTO_INCREMENT,
-                                     name VARCHAR(100),
-                                     email VARCHAR(100)
-);
